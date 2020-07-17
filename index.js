df58af01bdea213296d6a17b53b0fc74e7a66925
Princegxi <61604361+Princegxi@users.noreply.github.com>
2020-07-17 01:56:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+
+client.on("message", message => {
+
+if (message.content.startsWith(prefix + "dm")) {
+if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` **MESSAGE SENT** `); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready',()=>{
+    console.log(' Bot is online now!');
+    let statuses = [
+        "FAST HOUSTON",
+        "7ON7",
+        "WE RUN 7ON7" // U can add another
+        
+    ]
+    setInterval(function(){
+            let status = statuses[Math.floor(Math.random() * statuses.length)];
+            client.user.setActivity(status, {type:"WATCHING"})
+    
+        }, 3000) //Seconds to Random
+
+
+
+});
+
+client.login("Njk0MzU0MzkyMjI0MjM1NjEw.XoKaUw.rV1XVNtJmNicLc0DeFcFDvQrVQI");
